cfdf883aca0419ef4e2e8e2ae789e15b9f938cab
kigaa <78030218+flechi78@users.noreply.github.com>
2021-08-04 17:26:15 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,42 +1,92 @@
-/******************************
-project: GRADING SYTSEM       *
-Author : samsonkigaa& godfrey *
-Date :july                    *
-compiler:C99                  *
-******************************/
+/***********************************
+project:LIBRARY MANAGMENT SYSTEM   *
+AUTHOR: KIGAA ,GODFREY             *
+DATE: AUGUST                       *
+COMPLIER:C99                       *
+LICENSE;MIT                        *
+***********************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-   //declare variable
-
-    int mark;
-    char grade;
-
-    printf("Zetech university Grading system\n");
+    int action;
+    printf("COMMUNITY LIBRARY SYTEM!\n");
+    printf("WELCOME SIR GODFREY & SIR KIGAA\n");
+    //Nvigation system
     do{
-            printf("Enter a mark(0-100):");
-            scanf("%d",&mark);
+          printf("1. Add user\n");
+    printf("2. Add book\n");
+    printf("3. exit");
+    printf("Action(1-3:");
+    scanf("%d",&action);
 
-        if( mark<0){
+//validate
+    if(action<1 || action> 3)
+        {
+        printf("Invalid action!!!\n");
+        system("cls");
+        sleep(2000);
+}
+}while(action <1 || action >3);
+    execute_action (action);
+    return 0;
+}
 
-    }
-
-
-
-
-
-    }
-
-
-
-
-
+void execute_action (int action){
+switch(action){
+case 1:
+    printf("adding user\n");
+    break;
+case 2:
+    printf("adding book\n");
+    break;
+    case 3:
+    close();
+    break;
+    default :
+    printf("sorry i dont no how to execute that");
 
 }
+    void close(){
+        printf("thank you for using the system");
+        sleep(2000);
+        exit(0);
 }
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
